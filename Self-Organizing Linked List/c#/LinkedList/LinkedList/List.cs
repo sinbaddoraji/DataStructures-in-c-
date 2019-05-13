46448147182698457206e15a1534d9459f314d18
sinbaddoraji <40364978+sinbaddoraji@users.noreply.github.com>
2019-05-13 22:10:25 +0100

    Self-Organized Linked List in c#

--- Self-Organizing Linked List/c#/LinkedList/LinkedList/List.cs
+++ Self-Organizing Linked List/c#/LinkedList/LinkedList/List.cs
@@ -3,13 +3,13 @@
     internal class Node
     {
         public Node next = null;
+        public Node previous = null;
         public int value;
     }
 
     internal class List
     {
         public Node start = null;
-        public Node end = null;
 
         public void Insert(int value)
         {
@@ -18,15 +18,44 @@
                 value = value
             };
 
-            if (start == null)
+            if (start == null) start = node;
+            else Insert( node, start);
+
+        }
+
+        public void Insert(Node node, Node cur)
+        {
+            if (cur == start && node.value < cur.value)
             {
-                start = node;
-                end = node;
+                Node temp = node; // New node to be added to start
+                start.previous = null; // Remove node linking backwords
+                temp.next = start; //Link current start to be its next link
+
+                temp.next.previous = temp;
+
+                start = temp;
             }
             else
             {
-                end.next = node;
-                end = node;
+                if (cur.next == null) cur.next = node;
+                else
+                {
+                    while (true)
+                    {
+                        if (node.value > cur.next.value)
+                            cur = cur.next;
+                        else break;
+
+
+                        if (cur.next == null) break;
+                    }
+
+                    //Insert value between current and it's next value
+
+                    node.next = cur.next;
+                    cur.next = node;
+                    node.previous = cur;
+                }
             }
         }
 
